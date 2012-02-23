ffac4638dffd62dddd318298a32139b446d1d5e3
estill01 <ehs.stillman@gmail.com>
2012-02-23 14:54:22 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Batsignal::Application.config.secret_token = 'c3032f0b9d7e522609b2cecdeb983e26938455250be4aee8c407542ade858f51efbec54be08be1df0b5fd309e15d4b4db86f30b87300e9501cf3c06e7aa5bff9'
