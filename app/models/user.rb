1726da46b676aff315100aab42571092815360e4
estill01 <ehs.stillman@gmail.com>
2012-02-24 21:59:43 -0600

    minor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ActiveRecord::Base
+
   authenticates_with_sorcery!
 
   has_many :agendas
 
-  attr_accessible :name, :email
+  attr_accessible :username, :email, :phone_number, :password
+  attr_accessor :password
 
   validates :username, :uniqueness => true
+  validates :username, :presence => true
+
+  validates :phone_number, :presence => true
+
+  validates :password, :presence => true
   validates :password, :length => {:minimum => 4}
 
+  def over_18
+    @user.age >= Time.now - 18.years
+  end
 end
