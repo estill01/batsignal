8378b91160bf1aecab042bdba21d4f985cd9b6e8
estill01 <ehs.stillman@gmail.com>
2012-03-02 21:27:56 -0600

    successful left-side layout re-org

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # before_filter :get_vars 
-
-
-  def get_vars
-    @user = User.find(params[:id])
-    @agendas = @user.agendas.order("position")
-    @follows = @user.all_follows
-    if @user == current_user
-      @agenda = current_user.agendas.new
-    end
+  before_filter :left_side_data
+  
+  def left_side_data
+    if current_user 
+      @me = current_user 
+      @my_agendas = @me.agendas.find(:all)
+      @my_follows = @me.all_follows
+      @new_agenda = @me.agendas.new
+    end 
   end
+
 #     elsif  @user == current_user
 #       @agendas = @user.agendas.order("position")
 #       @follows = @user.all_follows
@@ -24,18 +24,4 @@ class ApplicationController < ActionController::Base
 #         @agenda = current_user.agendas.new
 #       end
 
-
-
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile/
-    end
-  end
-  helper_method :mobile_device?
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-  end
 end
